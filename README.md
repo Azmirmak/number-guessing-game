a395754ecdff7917159a8be60f0cd0b855f950c9
Azmirmak <mazmirkhan38@gmail.com>
2024-03-25 03:51:29 +0500

    Created number guessing game

---
+++ README.md
@@ -0,0 +1,2 @@
+# number-guessing-game
+This is just a number guessing game
---
+++ index.ts
@@ -0,0 +1,18 @@
+#! /usr/bin/env node 
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+
+const answer = await inquirer.prompt([
+  { message: "Please guess a number between 1-10: ",
+    name: "guessedNumber",
+    type: "number", 
+  },
+]);
+
+if(answer.guessedNumber === randomNumber) {
+    console.log("Congratulation! You guessed the right number");
+}else{
+    console.log("You guessed the wrong number");   
+}
