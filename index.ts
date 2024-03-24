412672432bf7ab595fb45cd43b0a694a57f39f17
Azmirmak <mazmirkhan38@gmail.com>
2024-03-25 04:01:02 +0500

    added console

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ import inquirer from "inquirer";
 
 const randomNumber = Math.floor(Math.random() * 10 + 1);
 
+console.log("Welcome to the most difficult game");
+
 const answer = await inquirer.prompt([
   { message: "Please guess a number between 1-10: ",
     name: "guessedNumber",
